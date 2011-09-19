60a33bce6dd2d503f3d79d2166836d2117730c2c
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2011-09-19 17:12:18 +0200

    declare the vizkit3d plugin in vizkit/ruby

---
+++ viz/vizkit_plugin.rb
@@ -0,0 +1,3 @@
+Vizkit::UiLoader.register_3d_plugin('VFHTreeViewer', 'vfh_star', 'VFHTree', 'updateVFHTree', 'updateTree')
+Vizkit::UiLoader.register_3d_plugin_for('VFHTreeViewer', "/wrappers/vfh_star/Tree", :updateVFHTree)
+
