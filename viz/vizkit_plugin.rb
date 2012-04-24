669a951a882dcfed76fbf9d445d908e0aeed1282
Janosch Machowinski <janosch.machowinski@dfki.de>
2012-04-24 16:58:55 +0200

    register vizkit plugin for /corridor_navigation/VFHStarDebugData_m

--- viz/vizkit_plugin.rb
+++ viz/vizkit_plugin.rb
@@ -4,6 +4,12 @@ Vizkit::UiLoader.register_3d_plugin_for('VFHTreeViewer', "/corridor_navigation/F
     plugin.updateTree(data.tree)
     plugin.updateSegment(data.horizon)
 end
+Vizkit::UiLoader.register_3d_plugin_for('VFHTreeViewer', "/corridor_navigation/VFHStarDebugData_m") do |plugin, data, port|
+    segment = []
+    segment << data.horizonOrigin + data.horizonVector * 0.5
+    segment << data.horizonOrigin - data.horizonVector * 0.5
+    plugin.updateSegment(segment)
+end
 
 Vizkit::UiLoader.register_3d_plugin('VFHLocalMapViewer', 'vfh_star', 'TraversabilityMapGenerator')
 Vizkit::UiLoader.register_3d_plugin_for('VFHLocalMapViewer', "/vfh_star/GridDump", :updateGridDump)
